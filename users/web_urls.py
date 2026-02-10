9679ba0c2b5fa8414f8487a48c1d2a77ab50b04a
webdevtech2030 <hemelorga@gmail.com>
2026-02-10 18:06:05 +0400

    Add root URL redirect to login page

--- users/web_urls.py
+++ users/web_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from .views import login_page
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="users-login-page", permanent=False)),
     path("login/", login_page, name="users-login-page"),
 ]
